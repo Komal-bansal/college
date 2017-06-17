891f43ad2891d0dbe0e306a060969107d74a8022
komal-bansal <komalb1408@gmail.com>
2017-06-16 19:39:27 -0700

    first commit

---
+++ js/ui-routes.js
@@ -0,0 +1,29 @@
+
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+
+$stateProvider
+
+    // HOME STATES AND NESTED VIEWS ========================================
+    .state('home', {
+        url: '/home',
+        templateUrl: '../pages/home.html'
+    })
+
+//    // nested list with custom controller
+//    .state('home.list', {
+//        url: '/list',
+//        templateUrl: 'partial-home-list.html',
+//        controller: function($scope) {
+//            $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+//        }
+//    })
+//
+//    // nested list with just some random string data
+//    .state('home.paragraph', {
+//        url: '/paragraph',
+//        template: 'I could sure use a drink right now.'
+//    })
